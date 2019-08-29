71091bd1a0717f44a7bafea8d85729a9f2387491
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-29 00:46:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 var recipes = new Object()
-
-function updateObjectWithKeyAndValue (object, key, value) {
-  return Object.assign({}, object, {[key]: 'value'})
-}
-
-function  updateObjectWithKeyAndValue (object, key, value){
-
-}
